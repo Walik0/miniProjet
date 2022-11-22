85476a51ce1872e51bb7853e397b6e96f8c5f53b
Walik0 <86592235+Walik0@users.noreply.github.com>
2022-11-22 15:53:11 +0100

    Add files via upload

--- Bateau.java
+++ Bateau.java
@@ -6,29 +6,117 @@ public class Bateau
 {
         int positionX;
         int positionY;
-        int typeBateau;
+        char typeBateau;
+        boolean verticale;
 
         public Bateau()
         {
 
         }
+
+        public static String typeBateau(int type)
+        {
+
+            String typeBateau = "";
+
+            switch(type)
+            {
+                case 1:
+                typeBateau = "*";
+                break;
+
+                case 2:
+                typeBateau = "°";
+                break;
+
+                case 3:
+                typeBateau = "&";
+                break;
+
+                case 4:
+                typeBateau = "%";
+                break;
+            }
+
+            return typeBateau;
+        }
+
+        public static Bateau placagBateau(Bateau bateau)
+        {
+
+
+            return bateau;
+        }
     
     public static void main(String args [])
     {
         Bateau bateau = new Bateau();
-        affichageGrille(bateau);
+        affichageGrille();
         
     }
 
-    public static void affichageGrille(Bateau bateau)
+    public static void initialisation()
     {
-        for (int i = 0; i < 10; i++) 
+        Bateau bateauJoueur1 = new Bateau();
+
+        bateauJoueur1 = saisiBateau();
+    }
+
+    public static Bateau saisiBateau()
+    {
+        Bateau bateau = new Bateau();
+
+        Ecran.afficher("quelle est la position X ?: ");
+        bateau.positionX = Clavier.saisirInt();
+
+        Ecran.afficher("quelle est la postition Y ?: ");
+        bateau.positionY = Clavier.saisirInt();
+
+        Ecran.afficher("quelle est le type du bateu ?: ");
+        bateau.typeBateau = Clavier.saisirChar();
+
+        bateau.verticale = orientation();
+        return bateau;
+    }
+
+    public static boolean orientation()
+    {
+        boolean orientation = true;
+
+        return orientation;
+    }
+
+    public static void affichageGrille()
+    {
+        //affiche une grille est change la case si il sagit d'un bateau
+
+        for (int X = 0; X < 10; X++) 
         {
-            for (int j = 0; j < 10; j++) 
+            for (int Y = 0; Y < 10; Y++) 
             {
-                Ecran.afficher("[ ]");    
+                if(estPosBateau(X,Y) && X < 10 && Y < 10)
+                {
+                    Ecran.afficher("[*]");
+                }else
+                {
+                    Ecran.afficher("[ ]");
+                }    
             } 
             Ecran.afficherln();   
         }
+    }
+
+
+    public static boolean estPosBateau(int X,int Y)
+    {
+        //retourne vrai si X et Y sont des coordonée de bateau
+        boolean estPosBateau = false;
+
+        if(X == 4 && Y == )
+        {
+            return true;
+        }
+
+        return estPosBateau;
     }
 }
